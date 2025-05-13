4bb50af87afb965a399c2bee56ccd05c65cc7e4c
Maximiliano Calahorra <maximilianocalahorra@gmail.com>
2025-05-12 21:31:11 -0300

    Delete api-apollo directory

--- api-apollo/src/index.ts
+++
@@ -1,8 +0,0 @@
-import app from "./server";
-
-//Definimos el puerto en el que se va a ejecutar la aplicación:
-const PORT = 3030;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on localhost:${PORT}`);
-});
